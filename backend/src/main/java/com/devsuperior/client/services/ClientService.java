cc945a7b57da1409868c966a82bc143e759108cb
NaianaCLima <naianacordeirodelima@gmail.com>
2021-12-17 15:28:27 -0300

    Seed, ClientDTO

--- backend/src/main/java/com/devsuperior/client/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/client/services/ClientService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.client.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.client.dto.ClientDTO;
 import com.devsuperior.client.entities.Client;
 import com.devsuperior.client.repositories.ClientRepository;
 
@@ -14,9 +17,10 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
-	public List<Client> findAll() {
-		return repository.findAll();
-
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 
 }
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_client (name, cpf, income, children) VALUES ('Naiana', '01018896403', 2.300, 1);
+INSERT INTO tb_client (name, cpf, income, children) VALUES ('Lucca', '01536978925', 0, 0);
+INSERT INTO tb_client (name, cpf, income, children) VALUES ('Loe', '12605603420', 2000, 2);
