dc2f6e1ae5255d3740f71c4556563d330ad261b5
NaianaCLima <naianacordeirodelima@gmail.com>
2021-12-20 10:42:34 -0300

    Post Client

--- backend/src/main/java/com/devsuperior/client/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/client/resources/ClientResource.java
@@ -1,13 +1,17 @@
 package com.devsuperior.client.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.client.dto.ClientDTO;
 import com.devsuperior.client.services.ClientService;
@@ -30,6 +34,13 @@ public class ClientResource {
 		ClientDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 
+	}
+	@PostMapping // inseri um novo recurso
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();//insere um objeto
+		return ResponseEntity.created(uri).body(dto);
+		
 	}
 
 }
